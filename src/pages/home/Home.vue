c19f7b9bfbb551dadb30e94c03d1154efc201bd8
zaoyubo <1525637754@qq.com>
2020-03-05 20:54:31 +0800

    init

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <home-header></home-header>
+  </div>
+</template>
+
+<script>
+import HomeHeader from './components/Header'
+export default {
+  name: 'Home',
+  components: {
+    HomeHeader
+  }
+}
+</script>
+
+<style>
+  .home{
+    font-size: 50px;
+  }
+</style>
