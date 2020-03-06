11cadda43fbd99592e145874fc7ac38e725433e2
zaoyubo <1525637754@qq.com>
2020-03-06 20:54:21 +0800

    add city list

---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class="list">
+    <div class="area">
+      <div class="title border-topbottom">当前城市</div>
+      <div class="button-list">
+        <div class="button-wrapper">
+          <div class="button">北京</div>
+        </div>
+        <div class="button-wrapper">
+          <div class="button">北京</div>
+        </div>
+        <div class="button-wrapper">
+          <div class="button">北京</div>
+        </div>
+      </div>
+    </div>
+    <div class="area">
+      <div class="title border-topbottom">热门城市</div>
+      <div class="button-list">
+        <div class="button-wrapper">
+          <div class="button">北京</div>
+        </div>
+        <div class="button-wrapper">
+          <div class="button">北京</div>
+        </div>
+        <div class="button-wrapper">
+          <div class="button">北京</div>
+        </div>
+      </div>
+    </div>
+    <div class="area">
+      <div class="title border-topbottom">A</div>
+      <div class="item-list">
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+      </div>
+    </div>
+    <div class="area">
+      <div class="title border-topbottom">A</div>
+      <div class="item-list">
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+      </div>
+    </div>
+    <div class="area">
+      <div class="title border-topbottom">A</div>
+      <div class="item-list">
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+      </div>
+    </div>
+    <div class="area">
+      <div class="title border-topbottom">A</div>
+      <div class="item-list">
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'List'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .border-topbottom
+    &:before
+      border-color: #ccc
+    &:after
+      border-color: #ccc
+  .border-bottom
+    &:before
+      border-color: #ccc
+  .list
+    position absolute
+    top 1.58rem
+    right 0
+    left 0
+    bottom 0
+    background red
+    overflow hidden
+    .title
+      line-height .44rem
+      background #eee
+      padding-left .2rem
+      color #666
+      font-size .26rem
+    .button-list
+      padding .1rem .6rem .1rem .1rem
+      overflow hidden
+      .button-wrapper
+        float left
+        width 33.33%
+        .button
+          padding .1rem 0
+          text-align center
+          margin .1rem
+          border .02rem solid #ccc
+          border-radius .06rem
+    .item-list
+      .item
+        line-height .76rem
+        padding-left .2rem
+</style>
