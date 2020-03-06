565db4f8e5a34d87bb64018f68a191419e927c9c
zaoyubo <1525637754@qq.com>
2020-03-06 12:42:04 +0800

    add home recommed

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <div class="recommed-title">热销推荐</div>
+    <ul>
+      <li class="item border-bottom" v-for="recommedItem in recommedList" :key="recommedItem.id">
+        <img class="item-img" :src="recommedItem.imgUrl" />
+        <div class="item-info">
+          <p class="item-title">{{recommedItem.title}}</p>
+          <p class="item-desc">{{recommedItem.desc}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommedList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1607/ce/ce94534274b3bd6db4.water.jpg_200x200_91a2f180.jpg',
+        title: '清华大学',
+        desc: '北京市海淀区,北京高等学府'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1607/ce/ce94534274b3bd6db4.water.jpg_200x200_91a2f180.jpg',
+        title: '清华大学',
+        desc: '北京市海淀区,北京高等学府'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1607/ce/ce94534274b3bd6db4.water.jpg_200x200_91a2f180.jpg',
+        title: '清华大学',
+        desc: '北京市海淀区,北京高等学府'
+      }, {
+        id: '0004',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1607/ce/ce94534274b3bd6db4.water.jpg_200x200_91a2f180.jpg',
+        title: '清华大学',
+        desc: '北京市海淀区,北京高等学府'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .recommed-title
+    margin-top .2rem
+    line-height .8rem
+    background #eee
+    text-indent .2rem
+  .item
+    display flex
+    height 1.9rem
+    .item-img
+      width 1.7rem
+      height 1.7rem
+      padding .1rem
+    .item-info
+      flex 1
+      padding .1rem
+      min-width 0
+      .item-title
+        ellipsis()
+        line-height .54rem
+        font-size .32rem
+      .item-desc
+        ellipsis()
+        line-height .4rem
+        color #CCCCCC
+      .item-button
+        background #ff9300
+        padding 0 .1rem
+        border-radius .06rem
+        margin-top .16rem
+        color #fff
+        line-height .44rem
+</style>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <div class="recommed-title">周末去哪儿</div>
+    <ul>
+      <li class="item border-bottom" v-for="recommedItem in recommedList" :key="recommedItem.id">
+        <div class="item-img-wrapper">
+          <img class="item-img" :src="recommedItem.imgUrl" />
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{recommedItem.title}}</p>
+          <p class="item-desc">{{recommedItem.desc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      recommedList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1603/6d/2f67ae0659f41f.jpg_r_640x214_bf6cbd0b.jpg',
+        title: '北京赏花好地方',
+        desc: '乱花渐欲迷人眼，京城赏花大搜索'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1603/6d/2f67ae0659f41f.jpg_r_640x214_bf6cbd0b.jpg',
+        title: '北京赏花好地方',
+        desc: '乱花渐欲迷人眼，京城赏花大搜索'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1603/6d/2f67ae0659f41f.jpg_r_640x214_bf6cbd0b.jpg',
+        title: '北京赏花好地方',
+        desc: '乱花渐欲迷人眼，京城赏花大搜索'
+      }, {
+        id: '0004',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1603/6d/2f67ae0659f41f.jpg_r_640x214_bf6cbd0b.jpg',
+        title: '北京赏花好地方',
+        desc: '乱花渐欲迷人眼，京城赏花大搜索'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .recommed-title
+    margin-top .2rem
+    line-height .8rem
+    background #eee
+    text-indent .2rem
+  .item-img-wrapper
+    height 0
+    overflow hidden
+    padding-bottom 33.9%
+  .item-img
+    width 100%
+  .item-info
+    flex 1
+    padding .1rem
+    min-width 0
+    .item-title
+      ellipsis()
+      line-height .54rem
+      font-size .32rem
+    .item-desc
+      ellipsis()
+      line-height .4rem
+      color #CCCCCC
+</style>
