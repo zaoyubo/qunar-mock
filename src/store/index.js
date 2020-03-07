ca9f851377f80cf0ee69ab2245121147c436167c
zaoyubo <1525637754@qq.com>
2020-03-07 15:34:26 +0800

    use localstorage to transmit currentcity

--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import state from './state'
+import mutations from './mutatiosn.js'
+import actions from './actions.js'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-    city: '上海'
-  },
-  actions: {
-    changeCity (ctx, city) {
-      ctx.commit('changeCity', city)
-    }
-  },
-  mutations: {
-    changeCity (state, city) {
-      state.city = city
-    }
-  }
+  state,
+  actions,
+  mutations
 })
